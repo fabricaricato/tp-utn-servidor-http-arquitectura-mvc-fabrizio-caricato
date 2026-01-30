980798ff5e06735fcda026a6e771ad39e1a8008c
Fabrizio <fabriziocaricato1@gmail.com>
2026-01-30 00:57:46 -0300

    CRUD en los controladores

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { connectDb } from "./config/mongodb.js"
 import express from "express"
 import cors from "cors"
 import { config } from "dotenv"
-import { getUsers, createUser } from "./controllers/user.controller.js"
+import { getUsers, createUser, deleteUser, updateUser } from "./controllers/user.controller.js"
 config()
 
 const PORT = process.env.PORT
@@ -12,8 +12,11 @@ const servidor = express()
 servidor.use(express.json())
 servidor.use(cors())
 
+// ENDPOINTS
 servidor.get("/users", getUsers)
 servidor.post("/users", createUser)
+servidor.patch("/users", updateUser)
+servidor.delete("/users", deleteUser)
 
 // CONEXIÓN Y ESCUCHA DEL PUERTO
 servidor.listen(PORT, () => {
