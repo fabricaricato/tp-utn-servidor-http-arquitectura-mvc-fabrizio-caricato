8bcb8a2a10f523e9005b8762c50ed848f8730ca2
Fabrizio <fabriziocaricato1@gmail.com>
2026-02-03 21:35:15 -0300

    condicion para contraseña y hasheo en register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js"
+import bcrypt from "bcryptjs";
 
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
@@ -7,17 +8,23 @@ const register = async (req, res) => {
     const { username, email, password } = req.body
 
     if (!username || !email || !password) {
-      return res.status(500).json({success: false, error: "debe completar todos los campos para poder registrarse"})
+      return res.status(400).json({success: false, error: "debe completar todos los campos para poder registrarse"})
     }
 
     if (!emailRegex.test(email) || (!email.endsWith(".com"))) {
       return res.status(400).json({success: false, error: "debe ingresar un mail valido"})
     }
 
+    if (password.length < 4) {
+      return res.status(400).json({ success: false, error: "la contraseña debe contar al menos con 5 caracteres" })
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = await User.create({
       username,
       email,
-      password
+      password: passwordHash
     })
 
     return res.status(200).json({success: true, data: newUser})
@@ -29,49 +36,3 @@ const register = async (req, res) => {
 }
 
 export {register}
-
-// const getUsers = async (req, res) => {
-//   try {
-//     const fetchedUsers = await User.find()
-//     return res.status(201).json({success: true, data: fetchedUsers})
-//   } catch (error) {
-//     return res.status(500).json({success: false, error: error.message})
-//   }
-// }
-
-// const createUser = async (req, res) => {
-//   try {
-//     const {username, email, password} = req.body
-//     const newUser = await User.create({
-//       username,
-//       email,
-//       password
-//     })
-//     return res.status(201).json({success: true, data: newUser})
-//   } catch (error) {
-//     return res.status(500).json({success: false, error: error.message})
-//   }
-// }
-
-// const updateUser = async (req, res) => {
-//   try {
-//     const id = req.params.id
-//     const updates = req.body
-//     const updatedUser = await User.findByIdAndUpdate(id, updates, { new: true })
-//     return res.status(201).json({success: true, data: updatedUser})
-//   } catch (error) {
-//     return res.status(500).json({success: false, error: error.message})
-//   }
-// }
-
-// const deleteUser = async (req, res) => {
-//   try {
-//     const id = req.params.id
-//     const deletedUser = await User.findByIdAndDelete(id)
-//     return res.status(201).json({success: true, data: deletedUser})
-//   } catch (error) {
-//     return res.status(500).json({success: false, error: error.message})
-//   }
-// }
-
-// export {getUsers, createUser, updateUser, deleteUser}
